3e1b768d56acdf8633e2488fe6189396a896ad3e
JlewisPk <lewis4692@hotmail.com>
2018-11-24 02:12:58 +1300

    done

---
+++ src/App.tsx
@@ -0,0 +1,325 @@
+import * as React from 'react';
+import Modal from 'react-responsive-modal';
+import * as Webcam from "react-webcam";
+import './App.css';
+import BillDetail from './components/BillDetail';
+import ItemList from './components/ItemList';
+import lewisBillLogo from './lewisBillLogo.png';
+
+
+interface IState {
+	authenticated: boolean,
+	billProduced: any[],
+	currentItem: any,
+	edit: boolean,
+	height: any,
+	items: any[],
+	open: boolean,
+	predictionResult: any,
+	refCamera: any
+	uploadFileList: any,
+	warning: any,
+	width: any,
+}
+
+class App extends React.Component<{}, IState> {
+	constructor(props: any) {
+        super(props)
+        this.state = {
+			authenticated: false,
+			billProduced: [],
+			currentItem: {
+				"height": "700",
+				"itemCount": 1,
+				"itemId": 1,
+				"itemName": "Pork Belly",
+				"itemPrice": 28,
+				"itemURL": "https://billplzblob.blob.core.windows.net/itemimages/porkbelly.jpg",
+				"uploaded": "11/10/2018 10:09:52 PM",
+				"width": "700"
+			  },
+			edit: false,
+			height: 0,
+			items: [],
+			open: false,
+			predictionResult: null,
+			refCamera: React.createRef(),
+			uploadFileList: null,
+			warning: '',
+			width: 0,
+		}     	
+		this.selectNewItems = this.selectNewItems.bind(this)
+		this.fetchItems = this.fetchItems.bind(this)
+		this.handleFileUpload = this.handleFileUpload.bind(this)
+		this.uploadItem = this.uploadItem.bind(this)
+		this.clearBill = this.clearBill.bind(this)
+		this.authenticate = this.authenticate.bind(this)
+		this.getFaceRecognitionResult = this.getFaceRecognitionResult.bind(this)
+		this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
+		this.fetchItems("")
+	}
+	public componentDidMount() {
+
+		this.setState({width:window.innerWidth, height:window.innerHeight})
+	}
+	public render() {
+		const { open, edit } = this.state;
+		// this.state.width = window.innerWidth;
+		// this.state.height = window.innerHeight;
+		window.addEventListener('resize', this.updateWindowDimensions);
+		
+		return (
+		<div>
+			<div className="header-wrapper">
+				<div className="container header">
+					{edit? 
+						this.state.width>950?
+						null
+						:
+						<div className="btn btn-primary btn-action extra-adding2" onClick={this.toggleEdit}>Cancel</div>
+							:
+						this.state.width>950?
+						null
+						:
+						<div className="btn btn-primary btn-action extra-adding2" onClick={this.toggleEdit}>Edit</div>
+					}
+					<img src={lewisBillLogo} height='40'/>
+					&nbsp; Lewis's Billing Helper! &nbsp;
+					{edit? 
+						this.state.width>950?
+						<div className="btn btn-primary btn-action btn-add extra-adding" onClick={this.toggleEdit}>Cancel Edit</div>
+						:
+						null
+							:
+						this.state.width>950?
+						<div className="btn btn-primary btn-action btn-add extra-adding" onClick={this.toggleEdit}>Edit List</div>
+						:
+						null
+					}
+					{this.state.width>950?
+						<div className="btn btn-primary btn-action btn-add" onClick={this.onOpenModal}>Add Item</div>
+						:
+						<div className="btn btn-primary btn-action btn-add" onClick={this.onOpenModal}>Add</div>
+					}
+				</div>
+			</div>
+			{(!this.state.authenticated) ?
+			<Modal open={!this.state.authenticated} onClose={this.authenticate} closeOnOverlayClick={false} showCloseIcon={false} center={true}>
+				<div>
+				<Webcam
+					className="webcam"
+					style={{width:'100%', height:'100%', }}
+					screenshotFormat="image/jpeg"
+					ref={this.state.refCamera}
+				/>
+				<div className="row nav-row login">
+					<div className="btn btn-primary bottom-button" onClick={this.authenticate}>Login</div>
+				</div>
+				</div>
+			</Modal> 
+			: 
+			<div>
+				{this.state.width>950? 
+				<div className="container">
+					{this.state.width>950?
+						<div className="row">
+							<div className="col-7">
+								<BillDetail currentItem={this.state.currentItem} selectNewItems={this.selectNewItems} clearBill={this.clearBill} billProduced={this.state.billProduced} />
+							</div>
+							<div className="col-5">
+								<ItemList billProduced={this.state.billProduced} edit={this.state.edit} items={this.state.items} selectNewItems={this.selectNewItems} searchByName={this.fetchItems}/>
+							</div>
+						</div>
+					:
+						<div className="row center">
+							<div className="col-25 center">
+								<ItemList billProduced={this.state.billProduced} edit={this.state.edit} items={this.state.items} selectNewItems={this.selectNewItems} searchByName={this.fetchItems}/>
+							</div>
+						</div>
+					}
+				</div>
+				:
+				<div className="container pad-remover">
+					{this.state.width>950?
+						<div className="row">
+							<div className="col-7">
+								<BillDetail currentItem={this.state.currentItem} selectNewItems={this.selectNewItems} clearBill={this.clearBill} billProduced={this.state.billProduced} />
+							</div>
+							<div className="col-5">
+								<ItemList billProduced={this.state.billProduced} edit={this.state.edit} items={this.state.items} selectNewItems={this.selectNewItems} searchByName={this.fetchItems}/>
+							</div>
+						</div>
+					:
+						<div className="row center">
+							<div className="col-25 center">
+								<ItemList billProduced={this.state.billProduced} edit={this.state.edit} items={this.state.items} selectNewItems={this.selectNewItems} searchByName={this.fetchItems}/>
+							</div>
+						</div>
+					}
+				
+				</div>
+				}
+		<Modal open={open} onClose={this.onCloseModal}>
+			<form>
+				<div className="form-group">
+					<label>Item Name</label>
+					<input type="text" className="form-control" id="item-name-input" placeholder="Enter Item Name" />
+					<small className="form-text text-muted">You can edit any item later</small>
+					<small className="form-text text-muted">Item Name is used for search</small>
+				</div>
+				<div className="form-group">
+					<label>Item Price</label>
+					<input type="text" className="form-control" id="item-price-input" placeholder="Enter Price" />
+					<small className="form-text text-muted">Item Price is used for bill calculation</small>
+				</div>
+				<div className="form-group">
+					<label>Image</label>
+					<input type="file" onChange={this.handleFileUpload} className="form-control-file" id="meme-image-input" />
+					<small className="form-text text-muted text-warning">{this.state.warning}</small>
+				</div>
+
+				<button type="button" className="btn" onClick={this.uploadItem}>Add Item!</button>
+			</form>
+		</Modal></div>
+			}
+		</div>
+		);
+	}
+	private updateWindowDimensions() {
+		this.setState({ width: window.innerWidth, height: window.innerHeight });
+	  }
+	// Call custom vision model
+private getFaceRecognitionResult(image: string) {
+	const url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/1f3378e0-8e35-43d1-8d15-866469694af6/image?iterationId=503c9065-d287-4488-b443-02d720f024ed"
+	if (image === null) {
+		return;
+	}
+	const base64 = require('base64-js');
+	const base64content = image.split(";")[1].split(",")[1]
+	const byteArray = base64.toByteArray(base64content);
+	fetch(url, {
+		body: byteArray,
+		headers: {
+			'Content-Type': 'application/octet-stream',
+			'Prediction-Key': '24ef251ab7e44dc7a89f3cdd85165233',
+			'cache-control': 'no-cache',
+		},
+		method: 'POST'
+	})
+		.then((response: any) => {
+			if (response.ok) {
+				response.json().then((json: any) => {
+					this.setState({predictionResult: json.predictions[0] })
+					if (this.state.predictionResult.probability > 0.7) {
+						this.setState({authenticated: true})
+					} else {
+						this.setState({authenticated: false})
+						
+					}
+				})
+			} else {
+				response.json().then((json: any) => {
+					// console.log(json.predictions[0])
+				})
+			}
+		})
+}
+	private authenticate() { 
+		const screenshot = this.state.refCamera.current.getScreenshot();
+		this.getFaceRecognitionResult(screenshot);
+	}
+	private clearBill = () => {
+		this.state.billProduced.splice(0,this.state.billProduced.length)
+		// location.reload()
+                this.forceUpdate()
+    }
+	private toggleEdit = () => {
+		this.setState({ edit: !this.state.edit });
+	  };
+	
+	// Modal open
+	private onOpenModal = () => {
+		this.setState({ open: true });
+	  };
+	
+	// Modal close
+	private onCloseModal = () => {
+		this.setState({ open: false, warning:'' });
+	};
+	
+	// Change selected meme
+	private selectNewItems(newItem: any) {
+		this.setState({
+			billProduced: newItem
+		})
+	}
+
+	private fetchItems(itemName: any) {
+		let url = "https://billplzapi.azurewebsites.net/api/items"
+		if (itemName !== "") {
+			url += "/itemName?itemName=" + itemName
+		}
+		fetch(url, {
+			method: 'GET'
+		})
+		.then(res => res.json())
+		.then(json => {
+			let currentItem = json[0]
+			if (currentItem === undefined) {
+				currentItem = {
+					"height": "",
+					"itemCount": 0,
+					"itemName": 'No matching ' + itemName + ' is not found D:',
+					"itemPrice": 28,
+					"itemURL": "",
+					"uploaded": "",
+					"width": ""
+				  }
+			}
+			this.setState({
+				currentItem,
+				items: json
+			})
+		});
+	}
+	private handleFileUpload(fileList: any) {
+		this.setState({
+			uploadFileList: fileList.target.files
+		})
+	}
+	private uploadItem() {
+		const itemName = document.getElementById("item-name-input") as HTMLInputElement
+		const itemPrice = document.getElementById("item-price-input") as HTMLInputElement
+		const imageFile = this.state.uploadFileList!==null?this.state.uploadFileList[0]:null
+	
+		if (itemName === null || itemPrice === null || imageFile === null) {
+			this.setState({warning:'Make sure all fields are filled!'})
+		} else {
+			const ItemName = itemName.value
+			const ItemPrice = itemPrice.value
+			const url = "https://billplzapi.azurewebsites.net/api/items/upload"
+		
+			const formData = new FormData()
+			formData.append("itemName", ItemName)
+			formData.append("itemPrice", ItemPrice)
+			formData.append("image", imageFile)
+		
+			fetch(url, {
+				body: formData,
+				headers: {'cache-control': 'no-cache'},
+				method: 'POST'
+			})
+			.then((response : any) => {
+				if (!response.ok) {
+					// Error State
+					alert(response.statusText)
+				} else {
+					location.reload()
+				}
+			})
+		}
+	
+	}
+}
+
+export default App;
---
+++ src/components/ItemList.tsx
@@ -0,0 +1,261 @@
+import MediaStreamRecorder from 'msr';
+import * as React from "react";
+import Modal from 'react-responsive-modal';
+
+interface IProps {
+    billProduced: any[],
+    edit: boolean,
+    items: any[],
+    selectNewItems: any,
+    searchByName: any,
+}
+interface IState {
+	selectedItem: any,
+    open: boolean,
+    warning: any,
+}
+export default class ItemList extends React.Component<IProps, IState> {
+    constructor(props: any) {
+        super(props)   
+        this.state = {
+            open: false,
+            selectedItem: {
+				height: "700",
+				itemCount: 1,
+				itemId: 1,
+				itemName: "Pork Belly",
+				itemPrice: 28,
+				itemURL: "https://billplzblob.blob.core.windows.net/itemimages/porkbelly.jpg",
+                uploaded: "11/10/2018 10:09:52 PM",
+				width: "700"
+			  },
+              warning: ''
+        }
+        this.searchByName = this.searchByName.bind(this)
+        this.updateItem = this.updateItem.bind(this)
+        this.searchTagByVoice = this.searchTagByVoice.bind(this) 
+        // this.getAccessTokenForVoiceCognitive = this.getAccessTokenForVoiceCognitive.bind(this)
+    }
+	public render() {
+        const { open } = this.state;
+		return (
+			<div className="container item-list-wrapper">
+                <div className="row">
+                    <div className="input-group">
+                        <input type="text" onKeyPress={this.handleKeyPress} id="search-item-textbox" className="form-control" placeholder="Search By Item Name" />
+                        <div className="input-group-append">
+                            <div className="btn" onClick={this.searchTagByVoice}><i className="fa fa-microphone icon-color" /></div>
+                            <div className="btn btn-outline-secondary search-button" onClick = {this.searchByName}>Search</div>
+                        </div>
+                    </div>  
+                </div>
+                <div className="row item-list-table">
+                    <table className="table table-striped">
+                        <tbody>
+                            {this.createTable()}
+                        </tbody>
+                    </table>
+                </div>
+
+
+                
+                <Modal open={open} onClose={this.onCloseModal}>
+                    <form>
+                        <div className="form-group">
+                            <label>Item Name</label>
+                            <input type="text" className="form-control" id="item-edit-name-input" placeholder="Enter New Item Name"/>
+                            <small className="form-text text-muted">You can edit any item later</small>
+                        </div>
+                        <div className="form-group">
+                            <label>Item Price</label>
+                            <input type="text" className="form-control" id="item-edit-price-input" placeholder="Enter New Price"/>
+                            <small className="form-text text-muted text-warning">{this.state.warning}</small>
+                        </div>
+                        <button type="button" className="btn" onClick={this.updateItem}>Save</button>
+                    </form>
+                </Modal>
+            </div>
+		);
+    }
+    
+    private async searchTagByVoice() {
+        const mediaConstraints = {
+            audio: true,
+            video:false
+        }
+        const onMediaSuccess = (stream: any) => {
+            const mediaRecorder = new MediaStreamRecorder(stream);
+            mediaRecorder.mimeType = 'audio/wav'; // check this line for audio/wav
+            mediaRecorder.ondataavailable = (blob: any) => {
+                let accessToken: any;
+                fetch('https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken', {
+                    headers: {
+                        'Content-Length': '0',
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Ocp-Apim-Subscription-Key': '821da687c4c146e69ee3e671077cf367'
+                    },
+                    method: 'POST'
+                }).then(async(response: any) => {
+                    const resToken = await response.text()
+                    return resToken
+                }).then(async (response: any) => {
+                    accessToken = response
+                }).catch((error: any) => {
+                    // console.log("Error", error)
+                });
+                // posting audio
+                fetch('https://westus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US', {
+                    body: blob, // this is a .wav audio file    
+                    headers: {
+                        'Accept': 'application/json',
+                        'Authorization': 'Bearer ' + accessToken,
+                        'Content-Type': 'audio/wav;codec=audio/pcm; samplerate=16000',
+                        'Ocp-Apim-Subscription-Key': '821da687c4c146e69ee3e671077cf367'
+                    },    
+                    method: 'POST'
+                }).then((res: any) => {
+                    // console.log('res:  ', res)
+                    return res.json()
+                }).then((res: any) => {
+                    // console.log(res)
+                    const textBox = document.getElementById("search-item-textbox") as HTMLInputElement
+                    textBox.value = (res.DisplayText as string).slice(0, -1)
+                }).catch((error: any) => {
+                    // console.log("Error", error)
+                });
+                mediaRecorder.stop()
+            }
+            mediaRecorder.start(3000);
+        }
+        
+        function onMediaError(e: any) {
+            // console.error('media error', e);
+        }
+
+        navigator.getUserMedia(mediaConstraints, onMediaSuccess, onMediaError);
+    }
+    // private postAudio(blob: any) {
+        
+    // }
+    private handleKeyPress = (event: any) => {
+        if(event.key === 'Enter'){
+          this.searchByName()
+        }
+      }
+    // Construct table using meme list
+	private createTable() {
+        const table:any[] = []
+        const itemList = this.props.items;
+        if (itemList == null) {
+            return table
+        }
+
+        for (let i = 0; i < itemList.length; i++) {
+            const children = []
+            const item = itemList[i]
+            children.push(<td key={"itemId" + i}><img src={item.itemURL} style={{width:50, height:50}}/></td>)
+            children.push(<td key={"itemName" + i}><div className="bill-row-item" >{item.itemName}</div></td>)
+            children.push(<td key={"itemPrice" + i}><div className="bill-row-item" >$ {item.itemPrice}</div></td>)
+            children.push(<td key={"itemButtons" + i}>
+            {this.props.edit?
+                <div className="row meme-done-button">
+                    <div className="btn btn-primary btn-action" onClick={this.onOpenModal.bind(this,item)}>Edit </div>
+                    <div className="btn btn-primary btn-action" onClick={this.deleteItem.bind(this, item.itemId)}>Delete </div>
+                </div>
+                :
+                null
+            }
+            
+        </td>)
+            table.push(<tr key={i+""} className="item-row" id={i+""} onClick= {this.selectRow.bind(this, i)}>{children}</tr>)
+        }
+        return table
+    }
+    // Meme selection handler to display selected meme in details component
+    private async selectRow(index: any) {
+        const selectedItem = this.props.items[index]
+        let addNew = true
+        await this.props.billProduced.map((itemInBill, index2) => {
+            if (itemInBill.itemName === selectedItem.itemName) {
+                addNew = false
+                this.props.billProduced[index2].itemCount +=1;
+            }
+        })
+        if (addNew) {
+            this.props.billProduced.push(selectedItem)
+        }
+        this.props.selectNewItems(this.props.billProduced)
+    }
+
+    // Search meme by tag
+    private searchByName() {
+        const textBox = document.getElementById("search-item-textbox") as HTMLInputElement
+        if (textBox === null) {
+            return;
+        }
+        const tag = textBox.value 
+        this.props.searchByName(tag)  
+    }
+
+    // Modal Open
+    private onOpenModal = (item:any) => {
+        this.setState({ open: true, selectedItem:item },()=> {
+            this.forceUpdate()
+        });
+	  };
+    
+    // Modal Close
+    private onCloseModal = () => {
+		this.setState({ open: false, warning:'' });
+    };
+    private updateItem(){
+        const newItemName = document.getElementById("item-edit-name-input") as HTMLInputElement
+        const newPriceName = document.getElementById("item-edit-price-input") as HTMLInputElement
+    
+        if (newItemName.value === '' || newPriceName.value === '') {
+			this.setState({warning:'Make sure all fields are filled!'})
+        } else {
+            const url = "https://billplzapi.azurewebsites.net/api/items/" + this.state.selectedItem.itemId
+            const jsonBody = {
+                "height": this.state.selectedItem.height,
+                "itemCount": 1,
+                "itemId": this.state.selectedItem.itemId,
+                "itemName": newItemName.value,
+                "itemPrice": newPriceName.value,
+                "itemURL": this.state.selectedItem.itemURL,
+                "uploaded": this.state.selectedItem.uploaded,
+                "width": this.state.selectedItem.width
+            }
+            fetch(url, {
+                body: JSON.stringify(jsonBody),
+                headers: {'cache-control': 'no-cache','Content-Type': 'application/json'},
+                method: 'PUT'
+            })
+            .then(async (response : any) => {
+                if (!response.ok) {
+                    // Error State
+                    alert(response.statusText + " " + url)
+                } else {
+                    location.reload()
+                }
+            })
+        }
+        
+    }
+    private deleteItem(id: any) {
+        const url = "https://billplzapi.azurewebsites.net/api/items/" + id
+    
+        fetch(url, {
+            method: 'DELETE'
+        })
+        .then((response : any) => {
+            if (!response.ok) {
+                // Error Response
+                alert(response.statusText)
+            }
+            else {
+                location.reload()
+            }
+        })
+    }
+}
